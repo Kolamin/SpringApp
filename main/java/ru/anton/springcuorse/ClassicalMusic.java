8ca8275ba54cccd6435bfed9e491fa9ccb0c000e
Kolamin <gofranich@gmail.com>
2020-07-17 16:46:24 +0600

    lesson 12

--- main/java/ru/anton/springcuorse/ClassicalMusic.java
+++ main/java/ru/anton/springcuorse/ClassicalMusic.java
@@ -1,12 +1,25 @@
 package ru.anton.springcuorse;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 import org.springframework.stereotype.Component;
 
 /**
  * @author Neil Alishev
  */
 @Component
+
 public class ClassicalMusic implements Music {
+
+    @PostConstruct
+    public void doMyInit(){
+        System.out.println("Do my initialization");
+    }
+    @PreDestroy
+    public void doMyDestroy(){
+        System.out.println("Doing my destruction");
+    }
     @Override
     public String getSong() {
         return "Hungarian Rhapsody";
